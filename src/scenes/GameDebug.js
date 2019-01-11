099ee6ec71f788d9aaf96d620d8915ec2e7696ff
T <tyson_ngo@hotmail.com>
2019-01-11 17:31:18 -0500

    modified debug menu

--- src/scenes/GameDebug.js
+++ src/scenes/GameDebug.js
@@ -6,7 +6,7 @@ window.debug = {
 	showFPS: true,
 	infiniteMeter: true,
 	invincible: true,
-	_playerHit: Player.prototype.hit
+	_playerHit: Player.prototype.hit,
 }
 
 // html menu to toggle various flags
@@ -15,6 +15,7 @@ debugForm.id = 'debugForm'
 debugForm.innerHTML = '';
 debugForm.style.position = 'fixed'
 debugForm.style.background = 'rgba(255,255,255,0.85)'
+// toggles
 for (let k in window.debug){
 	if (k.startsWith('_')) continue;
 	debugForm.innerHTML += `
@@ -23,11 +24,20 @@ for (let k in window.debug){
 		onclick = '(function(e){window.debug[e.id] = e.checked})(this)' />
 	${k}<label>`
 }
+// xyline
+debugForm.innerHTML += `
+<input type='Number' id='x' min='0' max='999' />
+<input type='Number' id='y' min='0' max='999' />
+`
 document.body.appendChild(debugForm);
 
 module.exports = function(GameScene){
 
 	GameScene.prototype.create = decorate(GameScene.prototype.create, function(){
+		let red = {
+			lineStyle: {color : 0xff0000},
+			fillStyle: {color: 0xff0000, alpha: 0.2}
+		};
 		window.debug._boss = this.boss;
 		debugForm.innerHTML += '<h5>BOSS PATTERNS</h5>'
 		this.boss.patterns.patterns.forEach((p, i) => {
@@ -47,15 +57,17 @@ module.exports = function(GameScene){
 		this.debugFps = this.add.text(10, this.game.canvas.height-30,'',
 		{fontFamily: 'Kong Text', fill: '#000', backgroundColor: '#fff'})
 		this.debugFps.depth = 1000;
-		this.debugHitbox = this.add.graphics({
-			lineStyle: {color : 0xff0000},
-			fillStyle: {color: 0xff0000, alpha: 0.2}
-		});
+		this.debugHitbox = this.add.graphics(red);
 		this.debugHitbox.depth = 1000;
+
+		this.XYLine = this.add.graphics(red);
+		this.XYLine.defaultStrokeWidth = 4;
+
 	})
 
 	GameScene.prototype.update = decorate(GameScene.prototype.update, function(){
 		this.debugHitbox.clear();
+		this.XYLine.clear();
 		if (window.debug.showHitboxes){
 			for (let k in this.objects){
 				this.objects[k].forEach(o => {
@@ -88,6 +100,20 @@ module.exports = function(GameScene){
 			this.player.hit = function(){};
 		} else {
 			this.player.hit = window.debug._playerHit;
+		}
+
+		let x = Number(document.getElementById('x').value);
+		let y = Number(document.getElementById('y').value);
+		if (x + y){
+			let height = this.game.canvas.height;
+			let width = this.game.canvas.width;
+			this.XYLine.beginPath();
+			this.XYLine.moveTo(0, y);
+			this.XYLine.lineTo(width, y);
+			this.XYLine.moveTo(x, 0);
+			this.XYLine.lineTo(x, height);
+			this.XYLine.closePath();
+			this.XYLine.strokePath();
 		}
 	})
 }
