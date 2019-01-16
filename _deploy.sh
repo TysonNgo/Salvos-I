74315df2cf848afd4837916171ded6948e427a3f
T <tyson_ngo@hotmail.com>
2019-01-16 15:48:38 -0500

    updated deployment script

--- _deploy.sh
+++ _deploy.sh
@@ -1,8 +1,11 @@
 npm run build
-mv dist/game.js ./
+cp dist/game.js ./
+cp node_modules/phaser/dist/phaser.min.js ./
+
 git add index.html
 git add game.js
 git add assets/
+git add phaser.min.js
 
 git commit --allow-empty-message -m ''
 git push origin gh-pages
